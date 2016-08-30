c833b9aea13471122352ab1f925e62a7126d08be
Vaycent <vaycentsun@gmail.com>
2016-08-30 17:23:51 +0800

    now can reset user values

--- app/src/main/java/vaycent/magicLog/customerControlHelper.java
+++ app/src/main/java/vaycent/magicLog/customerControlHelper.java
@@ -8,12 +8,27 @@ import java.io.File;
  * Created by Vaycent on 2016/4/16.
  */
 public class customerControlHelper {
-    public final static int PRINT_LOG_LEVEL=-10;
-    public final static File LOG_FILE_PATH=new File(Environment.getExternalStorageDirectory().getPath() + "/TestLog");
-    public final static String LOGFILE_FILTER_PRIORITY="i"; //v,d,i,w,e,s
-    public final static String LOGFILE_FILTER_TAG="Use_Here_02"; // ""for do not filter the tag
+    public static int PRINT_LOG_LEVEL=-10;
+    public static File LOG_FILE_PATH=new File(Environment.getExternalStorageDirectory().getPath() + "/TestLog");
+    public static String LOGFILE_FILTER_PRIORITY="i"; //v,d,i,w,e,s
+    public static String LOGFILE_FILTER_TAG="Use_Here_02"; // ""for do not filter the tag
 
 
+    public static void setPrintLogLevel(int level){
+        PRINT_LOG_LEVEL=level;
+    }
+
+    public static void setLogFilePath(String path){
+        LOG_FILE_PATH=new File(path);
+    }
+
+    public static void setLogfileFilterPriority(String priority){
+        LOGFILE_FILTER_PRIORITY= priority;
+    }
+
+    public static void setLogfileFilterTag(String tag){
+        LOGFILE_FILTER_TAG= tag;
+    }
 
 
 
