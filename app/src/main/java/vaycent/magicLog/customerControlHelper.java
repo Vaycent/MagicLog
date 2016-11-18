082a39914c701bb383944e9189537e13e2861e2f
Vaycent <vaycentsun@gmail.com>
2016-11-18 10:23:31 +0800

    reset function

--- app/src/main/java/vaycent/magicLog/customerControlHelper.java
+++ app/src/main/java/vaycent/magicLog/customerControlHelper.java
@@ -14,7 +14,21 @@ public class customerControlHelper {
     public static String LOGFILE_FILTER_TAG=""; // ""for do not filter the tag
 
 
+    public static void SetLogLevel(int level){
+        PRINT_LOG_LEVEL=level;
+    }
 
+    public static void SetLogPath(File file){
+        LOG_FILE_PATH=file;
+    }
+
+    public static void SetFilterPriority(String priority){
+        LOGFILE_FILTER_PRIORITY=priority;
+    }
+
+    public static void SetFilterTag(String tag){
+        LOGFILE_FILTER_TAG=tag;
+    }
 
 
 
